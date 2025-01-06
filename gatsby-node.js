d09b568c735f618548860d0a8f27b43c20bc3560
System Administrator <root@Sahils-MacBook-Air.local>
2025-01-06 15:05:10 +0530

    latest changes.

---
+++ gatsby-node.js
@@ -0,0 +1,13 @@
+exports.onCreateNode = ({ node, actions }) => {
+    const { createNodeField } = actions;
+  
+    if (node.internal.type === "MarkdownRemark") {
+      const slug = node.frontmatter.slug || slugify(node.frontmatter.title, { lower: true });
+      createNodeField({
+        node,
+        name: "slug",
+        value: slug,
+      });
+    }
+  };
+  
